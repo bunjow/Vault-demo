bbc45f370a57a03a677d262a0af9c0079235cf53
Matt Eng <matthew_eng@timeinc.com>
2020-10-26 15:50:48 -0400

    cleanup

--- Scripts/apache-plain.sh
+++ Scripts/apache-plain.sh
@@ -10,6 +10,8 @@ apt-get update
 apt-get -y install apache2
 apt-get -y install php-mysql
 apt-get -y install php
+apt-get -y mariadb-client-core-10.1
+apt-get -y install unzip
 
 # make sure nginx is started
 service apache2 restart
